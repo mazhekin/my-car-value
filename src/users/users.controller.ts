7dc4281452792d90d4e03558681794668f5f098f
Vasiliy Mazhekin <mazhekin@gmail.com>
2025-09-21 17:09:06 +0200

    users controller - apply serialize decorator to all controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,9 +7,7 @@ import {
   Param,
   Patch,
   Post,
-  Put,
   Query,
-  UseInterceptors,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
@@ -18,6 +16,7 @@ import { Serialize } from './interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
 
 @Controller('auth')
+@Serialize(UserDto)
 export class UsersController {
 
   constructor(private usersService: UsersService) {}
@@ -27,7 +26,6 @@ export class UsersController {
     this.usersService.create(body.email, body.password);
   }
 
-  @Serialize(UserDto)
   @Get('/:id')
   async findUser(@Param('id') id: string) {
     console.log('handler is running');
