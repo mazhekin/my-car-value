351422e96e9fa34b344a139858a317160b3ef08a
Vasiliy Mazhekin <mazhekin@gmail.com>
2025-09-24 21:38:25 +0200

    sing up - add auth service method signup

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,9 +1,12 @@
 import {
   BadRequestException,
-  Injectable,
-  NotFoundException,
+  Injectable
 } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { randomBytes, scrypt as _scrypt } from 'node:crypto';
+import { promisify } from 'util';
+
+const scrypt = promisify(_scrypt);
 
 @Injectable()
 export class AuthService {
@@ -18,10 +21,18 @@ export class AuthService {
     }
 
     // Hash the users password
+    // Generate a salt
+    const salt = randomBytes(8).toString('hex');
+    // Hash the salt and the password together
+    const hash = (await scrypt(password, salt, 32)) as Buffer;
+    // Join the hashed result and the salt together
+    const result = salt + '.' + hash.toString('hex');
 
     // Create a new user and save it
+    const user = await this.usersService.create(email, result);
 
     // return the user
+    return user;
   }
 
   signin() {}
