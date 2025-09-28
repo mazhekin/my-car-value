ab4c314ce3df47c657e3bb0fffd0ee3181946577
Vasiliy Mazhekin <mazhekin@gmail.com>
2025-09-28 20:21:18 +0200

    add current user decorator

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -16,6 +16,7 @@ import { Serialize } from './interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
 import { AuthService } from './auth.service';
 import { User } from './user.entity';
+import { CurrentUser } from './decorators/current-user.decorator';
 
 @Controller('auth')
 @Serialize(UserDto)
@@ -26,9 +27,14 @@ export class UsersController {
     private authService: AuthService
     ) {}
 
-  @Get('/whoami')
+  /*@Get('/whoami')
   whoAmI(@Session() session: any) {
     return this.usersService.findOne(session.userId);
+  }*/
+
+  @Get('/whoami')
+  whoAmI(@CurrentUser() user: string) {
+    return user;
   }
 
   @Post('/signout')
